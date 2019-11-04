28abf9724233ff4104737beeaed885eb34ff5698
Milan Desai <milan.desai@smartstream-stp.com>
2019-11-04 12:36:41 +0000

    added gitignore initial commit to print json data in readable format

--- commute.py
+++ commute.py
@@ -1,1 +1,15 @@
-#initial commit
+import json
+from datetime import datetime
+
+
+with open('sample.json') as json_file:
+  data = json.load(json_file)
+  for p in data['locations']:
+    dtm = p['timestampMs']
+    dtm2 = int(dtm)
+    dateandtime = datetime.fromtimestamp(dtm2/1000)
+    print('Date Time : ')
+    print(dateandtime)
+
+    print(p['latitudeE7'] / 10 ** 7)
+    print(p['longitudeE7'] / 10 ** 7)
